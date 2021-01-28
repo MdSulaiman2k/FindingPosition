ee263b140ef14763e52937e133ff4a98eb9ee5bb
md Sulaiman <77278345+MdSulaiman2k@users.noreply.github.com>
2021-01-28 13:54:36 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,57 +1,60 @@
-def checkvalue(word , word1) :
-    check = True 
-    for i in range(len(word)) :
-        for j in range(len(word1)):
-            if(word[i] == word[j]) :
-                chk = True 
-                break
-            else:
-                chk = False
-        if(not chk) :
+"""
+Find the Rank
+If we generate a list of all “words” made of letters G, I, N, R, T, and U in lexicographic order starting with GINRTU
+and ending with UTRNIG, what position in the list will be occupied by TURING?(Alan Turing was an English mathematician
+and computer scientist who, among other remarkable achievements, played a leading role in developing theoretical computer science.)
+
+"""
+import re
+
+def findingPosition(words, position):
+    reverse_word  =  words[::-1]
+    lenword = len(words)
+    factor = 1
+    count = 1
+    array = []
+    while(count<lenword) :
+        array.append(factor)
+        count += 1
+        factor = factor * count
+
+    count = count-2
+    num_of_position = factor
+    while(count >= 0) :
+        for i in position :
+            for j in reverse_word :
+
+                if(i==j) :
+                    reverse_word = re.sub(i,"",reverse_word)
+                    position =  re.sub(i,"",position)
+                    break
+                else :
+                    num_of_position = num_of_position - array[count]
+            count = count - 1
             break
-    return chk
-word = input("Enter the Word : ").upper()
-findposition = input("Enter the other word :  ").upper()
 
-arr = [0]*len(word)
-top = 0
+    return num_of_position
 
+def checkrepeat(words):
+    '''Returns True if words has no repeating characters; False otherwise.'''
+    return sorted(''.join(set(words))) == sorted(words)
 
-def push(s , top):
-    arr[top] = s
-    top += 1
+def checkEqual(words , find_pst ) :
+    '''Returns True if words and find_pst have the same characters'''
+    return sorted(words) == sorted(find_pst)
 
-def pop(s ,findposition ,top , c ) :
-    ck = False
-    if(top == 0) :
-        push(s , top)
-        return ck 
-    for i in range(top) :
-        if(arr[i] == findposition ):
-            ck = True
-            top -= 1
-    if(not ck):
-        push(s , top)
-    return ck
+ # returns true when its in lexicographic order (dictionary order)
+def lexicographicOrder(words)  :
+    if words.isalpha(): #check if string is alphabetic or not 
+        return all([c<d for c,d in zip(words,words[1:])]) 
+    return False
 
-if(len(word) == len(findposition) and checkvalue(word , findposition) ) :
-    factor =  1
-    factorial_list = [1]*(len(word)+1)
-    for i  in range( 1 , len(word)+1) :
-        factor *=  i
-        factorial_list[i] = factor 
-    i = 0
-    c = 0 
-    j = len(word)
-    cnt = 0 
-    for s in word[::-1] :
-        if(s != findposition[i]) :
-            if(pop(s , findposition[i]  , top , c)):
-              cnt += factorial_list[j]
-              top -= 1
-            else :
-              top += 1
-            c += 1
-        i += 1
-        j -= 1
-    print(factor - cnt)
+words  =  input("enter the word : ")
+
+find_pst = input("enter the which position u have to find :")
+
+if(len(words) == len(find_pst)  and checkEqual(words , find_pst)  and checkrepeat(words) and lexicographicOrder(words)) :
+    Position = findingPosition(words , find_pst)
+    print(Position)
+else :
+    print("your position is wrong")
